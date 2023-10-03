cab22cf17f871f981cc559cea3635618849699c8
Ejas <94757043+ej0x@users.noreply.github.com>
2023-10-03 23:12:54 +0530

    searchbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xff1D1617).withOpacity(0.11),
+                    blurRadius: 40,
+                    spreadRadius: 0.0),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
